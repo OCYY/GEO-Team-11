a7e3bc8490f72f5c800415b04b86fb05f70ce00a
OCYY <chenyu@users.noreply.github.com>
2020-11-20 15:14:25 +0800

    supply and demand of eldercare only

--- Project/app.R
+++ Project/app.R
@@ -29,11 +29,13 @@ population20_tidy <- population20 %>%
 
 pop_65above <- population20_tidy %>%
   mutate(`65_Above` = as.integer(rowSums(.[16:21]))) %>%
-  dplyr::select(PA, SZ, `65_Above`)
+  mutate_at(.vars = vars(PA,SZ), .funs = funs(toupper))%>%
+  dplyr::select(PA, SZ, `65_Above`) 
 
 mpsz <- st_read(dsn = "../data",
                 layer = "MP14_SUBZONE_WEB_PL")
 mpsz3414 <- st_transform(mpsz, 3414)
+
 community_club <- st_read("../data/community-clubs-kml.kml")
 community_club3414 <- st_transform(community_club, 3414)
 mpsz3414$`COMMUNITY_CLUBS` <- lengths(st_intersects(mpsz3414, community_club3414))
@@ -62,6 +64,30 @@ chas_supply <- mpsz3414 %>%
 
 chas_supply_nogeo <- st_set_geometry(chas_supply,NULL)
 
+mpsz3414_65Above <- left_join(mpsz3414,pop_65above,
+                              by=c("SUBZONE_N" = "SZ"))
+
+
+# mpsz3414_65Above_sp <- as_Spatial(mpsz3414_65Above)
+# 
+# # Converting the spatial point data frame into generic sp format
+# mpsz3414_65Above_sp <- as(mpsz3414_65Above_sp, "SpatialPoints")
+# 
+# #Converting the generic sp format into spatstat's ppp format
+# ppp_mpsz3414_65Above <- as(mpsz3414_65Above_sp, "ppp")
+# 
+# # Create owin to confine analysis 
+# mpsz_owin <- as(mpsz3414_65Above, "owin")
+# 
+# mpsz3414_65Above_ppp <- ppp_mpsz3414_65Above[mpsz_owin]
+# 
+# kde_mpsz3414_65Above <- density(mpsz3414_65Above_ppp, sigma=bw.diggle, edge=TRUE, kernel="gaussian")
+# gridded_kde_mpsz3414_65Above <- as.SpatialGridDataFrame.im(kde_mpsz3414_65Above)
+# kde_mpsz3414_65Above_raster <- raster(gridded_kde_mpsz3414_65Above)
+# projection(kde_mpsz3414_65Above_raster) <- CRS("+init=EPSG:3414")
+
+
+
 # Define UI
 ui <- fluidPage(theme = shinytheme("paper"),
                 navbarPage(
@@ -86,39 +112,50 @@ ui <- fluidPage(theme = shinytheme("paper"),
                              # Horizontal line ----
                              selectInput("mapset", "Choose a dataset:",
                                          choices = c("Community Clubs", "CHAS Clinics", "Eldercare Services")),
-                             
+
                              tags$hr(),
                              
                              # Input: Checkbox if file has header ----
-                             checkboxInput("header", "Header", TRUE),
+                             # checkboxInput("header", "Header", TRUE),
                              
                              # Input: Select separator ----
-                             radioButtons("sep", "Separator",
-                                          choices = c(Comma = ",",
-                                                      Semicolon = ";",
-                                                      Tab = "\t"),
-                                          selected = ","),
+                             # radioButtons("sep", "Separator",
+                             #              choices = c(Comma = ",",
+                             #                          Semicolon = ";",
+                             #                          Tab = "\t"),
+                             #              selected = ","),
                              
                              # Input: Select quotes ----
-                             radioButtons("quote", "Quote",
-                                          choices = c(None = "",
-                                                      "Double Quote" = '"',
-                                                      "Single Quote" = "'"),
-                                          selected = '"'),
+                             # radioButtons("quote", "Quote",
+                             #              choices = c(None = "",
+                             #                          "Double Quote" = '"',
+                             #                          "Single Quote" = "'"),
+                             #              selected = '"'),
                              
                              # Horizontal line ----
-                             tags$hr(),
+                             # tags$hr(),
                              
                              # Input: Select number of rows to display ----
-                             radioButtons("disp", "Display",
-                                          choices = c(Head = "head",
-                                                      All = "all"),
-                                          selected = "head")
+                             # radioButtons("disp", "Display",
+                             #              choices = c(Head = "head",
+                             #                          All = "all"),
+                             #              selected = "head")
                              
                            ),
-                           mainPanel(
-                             leafletOutput(outputId = "map",height = 660),
-                             tableOutput("contents")
+                           mainPanel(fluid=TRUE,
+                                     fluidRow(
+                                       column(6,class="well",
+                                              leafletOutput(
+                                                outputId = "supplymap"
+                                              )
+                                       ),
+                                       column(6,class="well",
+                                              leafletOutput(
+                                                outputId = "demandmap"
+                                              )
+                                       )
+                                     )
+                                     
                              
                            ) # mainPanel
                            
@@ -149,25 +186,44 @@ ui <- fluidPage(theme = shinytheme("paper"),
                   
                 ) # navbarPage
        
+                
 ) # fluidPage
 
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles()%>%
-      addMarkers(data=community_club$geometry)
+  # output$map <- renderLeaflet({
+  #   leaflet() %>%
+  #     addTiles()%>%
+  #     addMarkers(data=community_club$geometry)
+  # })
+  
+  datasetInput <- reactive({
+    switch(input$mapset,
+           "Population (65 Above)" = pop_65above,
+           "CHAS Clinics" = chas_supply_nogeo,
+           "Eldercare Services" = eldercare_supply_nogeo,
+           "Community Clubs" = community_club_supply_nogeo)
   })
   
+  
+  output$supplymap <- renderTmap (
+    tm_shape(mpsz3414)+
+      tm_polygons("ELDERCARE")
+  )
+  output$demandmap <- renderTmap (
+    tm_shape(mpsz3414_65Above)+
+      tm_polygons("65_Above")
+  )
+  
+  
+  
+  
   output$contents <- renderTable({
-    
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
-    
     req(input$file1)
-    
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
     tryCatch(
@@ -182,16 +238,12 @@ server <- function(input, output) {
         stop(safeError(e))
       }
     )
-    
     if(input$disp == "head") {
       return(head(df))
     }
     else {
       return(df)
     }
-    
-
-
   })
   
   datasetInput <- reactive({
