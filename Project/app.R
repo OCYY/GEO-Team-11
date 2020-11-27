e13b92bb02c6ae0c6fcd53b91c21b0a6c594aeec
OCYY <chenyu@users.noreply.github.com>
2020-11-27 15:33:09 +0800

    Added gyms

--- Project/app.R
+++ Project/app.R
@@ -34,10 +34,13 @@ community_club <- st_read("../data/community-clubs-kml.kml")
 community_club3414 <- st_transform(community_club, 3414)
 
 chas_clinics <- st_read("../data/chas-clinics-kml.kml")
-chas_clinics3413 <- st_transform(chas_clinics, 3414)
+chas_clinics3414 <- st_transform(chas_clinics, 3414)
 
 rc <- st_read(dsn = "../data", layer = "REsIDENTSCOMMITTEE")
-rc3414 <- st_transform(eldercare, 3414)
+rc3414 <- st_transform(rc, 3414)
+
+gym <- st_read("../data/gyms-sg-kml.kml")
+gym3414 <- st_transform(gym, 3414)
 
 chas_clinics_attributes <- lapply(X = 1:nrow(chas_clinics), 
                                   FUN = function(x) {
@@ -71,34 +74,57 @@ community_club_attributes <- community_club %>%
   bind_cols(bind_rows(community_club_attributes)) %>%
   dplyr::select(-Description)
 
+gym_attributes <- lapply(X = 1:nrow(gym), 
+                                    FUN = function(x) {
+                                      gym %>% 
+                                        slice(x) %>%
+                                        pull(Description) %>%
+                                        read_html() %>%
+                                        html_node("table") %>%
+                                        html_table(header = TRUE, trim = TRUE, dec = ".", fill = TRUE) %>%
+                                        as_tibble(.name_repair = ~ make.names(c("Attribute", "Value"))) %>% 
+                                        pivot_wider(names_from = Attribute, values_from = Value)
+                                    })
+
+gym_attributes <- gym %>%
+  bind_cols(bind_rows(gym_attributes)) %>%
+  dplyr::select(-Description)
+
 mpsz3414$`COMMUNITY_CLUBS` <- lengths(st_intersects(mpsz3414, community_club3414))
 mpsz3414$`ELDERCARE` <- lengths(st_intersects(mpsz3414, eldercare3414))
-mpsz3414$`CHAS_CLINIC`<- lengths(st_intersects(mpsz3414, chas_clinics3413))
-mpsz3414$`RC`<- lengths(st_intersects(mpsz3414, rc3413))
+mpsz3414$`CHAS_CLINIC`<- lengths(st_intersects(mpsz3414, chas_clinics3414))
+mpsz3414$`RC`<- lengths(st_intersects(mpsz3414, rc3414))
+mpsz3414$`GYM`<- lengths(st_intersects(mpsz3414, gym3414))
+
 mpsz3414_65Above <- left_join(mpsz3414, pop_65above, by=c("SUBZONE_N" = "SZ"))
 mpsz3414_65Above <- mpsz3414_65Above[!is.na(mpsz3414_65Above$'SENIOR_POPULATION'), ]
 #<----------------------------------------------for kde---------------------------------------->
 mpszB <- readOGR(dsn = "../data", layer="MP14_SUBZONE_WEB_PL")
 
-sp_chas <- as_Spatial(chas_clinics3413)
+sp_chas <- as_Spatial(chas_clinics3414)
 sp_eldercare <- as_Spatial(eldercare3414)
 sp_cc <- as_Spatial(community_club3414)
-sp_rc <- as_Spatial(rc3413)
+sp_rc <- as_Spatial(rc3414)
+sp_gym <- as_Spatial(gym3414)
+
 
 chas_sp <- as(sp_chas, "SpatialPoints")
 eldercare_sp <- as(sp_eldercare, "SpatialPoints")
 cc_sp <- as(sp_cc, "SpatialPoints")
 rc_sp <- as(sp_rc, "SpatialPoints")
+gym_sp <- as(sp_gym, "SpatialPoints")
 
 ppp_chas <- as(chas_sp, "ppp")
 ppp_eldercare <- as(eldercare_sp, "ppp")
 ppp_cc <- as(cc_sp, "ppp")
 ppp_rc <- as(rc_sp, "ppp")
+ppp_gym <- as(gym_sp, "ppp")
 
 ppp_chas_jit <- rjitter(ppp_chas, retry=TRUE, nsim=1, drop=TRUE)
 ppp_eldercare_jit <- rjitter(ppp_eldercare, retry=TRUE, nsim=1, drop=TRUE)
 ppp_cc_jit <- rjitter(ppp_cc, retry=TRUE, nsim=1, drop=TRUE)
 ppp_rc_jit <- rjitter(ppp_rc, retry=TRUE, nsim=1, drop=TRUE)
+ppp_gym_jit <- rjitter(ppp_gym, retry=TRUE, nsim=1, drop=TRUE)
 
 sp_mpszB <- as(mpszB, "SpatialPolygons")
 
@@ -108,6 +134,7 @@ chas_ppp = ppp_chas_jit[mpsz_owin]
 eldercare_ppp = ppp_eldercare_jit[mpsz_owin]
 cc_ppp = ppp_cc_jit[mpsz_owin]
 rc_ppp = ppp_rc_jit[mpsz_owin]
+gym_ppp = ppp_gym_jit[mpsz_owin]
 
 
 #<--------------------------------------------for gwc---------------------------------------->
@@ -163,6 +190,7 @@ ui <- fluidPage(theme = shinytheme("paper"),
                                                    "CHAS Clinics",
                                                    "Community Clubs",
                                                    "Eldercare Services",
+                                                   "Gyms",
                                                    "Residents Committee")),
                            
                            
@@ -176,6 +204,7 @@ ui <- fluidPage(theme = shinytheme("paper"),
                                        choices = c("CHAS Clinics", 
                                                    "Community Clubs",
                                                    "Eldercare Services",
+                                                   "Gyms",
                                                    "Residents Committee")),
                            mainPanel(width=10,fluid=TRUE,
                                      fluidRow(
@@ -196,6 +225,7 @@ ui <- fluidPage(theme = shinytheme("paper"),
                                                 choices = c("CHAS Clinics", 
                                                             "Community Clubs",
                                                             "Eldercare Services",
+                                                            "Gyms",
                                                             "Residents Committee"))
                              ),
                              column(4,
@@ -231,6 +261,7 @@ ui <- fluidPage(theme = shinytheme("paper"),
                                        choices = c("CHAS Clinics", 
                                                    "Community Clubs",
                                                    "Eldercare Services",
+                                                   "Gyms",
                                                    "Residents Committee")),
                            radioButtons("gwc_type", "GWC type:",
                                         c("Pearson Correlation" = "pearson",
@@ -241,7 +272,6 @@ ui <- fluidPage(theme = shinytheme("paper"),
                            mainPanel(fluid=TRUE,
                                      fluidRow(
                                        column(12,withSpinner(plotOutput(
-                                         # height = 600,
                                          outputId = "gwcmap"
                                        )))
                                      )
@@ -258,7 +288,9 @@ server <- function(input, output) {
            "CHAS Clinics" = chas_clinics_attributes,
            "Community Clubs" = community_club_attributes,
            "Eldercare Services" = eldercare,
-           "Residents Committee" = rc)
+           "Gyms" = gym,
+           "Residents Committee" = rc
+           )
   })
   
   output$table <- DT::renderDataTable(
@@ -290,16 +322,22 @@ server <- function(input, output) {
           tm_polygons("CHAS_CLINIC")
       )
     }
-    else if (x=="Eldercare Services"){
+    else if (x =="Eldercare Services"){
       output$supplymap <- renderTmap(
         tm_shape(mpsz3414_65Above)+
           tm_polygons("ELDERCARE")
       )
     }
-    else{
+    else if (x == "Residents Committee"){
       output$supplymap <- renderTmap(
         tm_shape(mpsz3414_65Above)+
           tm_polygons("RC")
+      )
+    }
+    else {
+      output$supplymap <- renderTmap(
+        tm_shape(mpsz3414_65Above)+
+          tm_polygons("GYM")
       )
     }
   })
@@ -324,9 +362,12 @@ server <- function(input, output) {
       facility= chas_ppp
     }else if(facility =="Eldercare Services"){
       facility= eldercare_ppp
-    }else {
+    }else if(facility =="Residents Committee") {
       facility= rc_ppp
+    }else {
+      facility= gym_ppp
     }
+    
     area = mpszB[mpszB@data$PLN_AREA_N == zone,]
     area_sp = as(area, "SpatialPolygons")
     area_owin = as(area_sp, "owin")
@@ -388,14 +429,14 @@ server <- function(input, output) {
     gwc_type<-input$gwc_type
     gwc_data<-input$gwc_data
     bandwidth <- input$bandwidth
-    localstats1 <- gwss(sp_mpsz3414_65Above,vars=c("SENIOR_POPULATION","CHAS_CLINIC","COMMUNITY_CLUBS","ELDERCARE","RC"),bw=bandwidth,adaptive = TRUE,quantile = TRUE)
+    localstats1 <- gwss(sp_mpsz3414_65Above,vars=c("SENIOR_POPULATION","CHAS_CLINIC","COMMUNITY_CLUBS","ELDERCARE","RC","GYM"),bw=bandwidth,adaptive = TRUE,quantile = TRUE)
     proj4string(localstats1$SDF) <- CRS("+init=epsg:3414")
     
     if(is.null(gwc_data))
       gwc_data<-character(0)
     if(is.null(gwc_type))
       gwc_type<-character(0)
-
+    
     if(gwc_data =="Community Clubs"){
       if(gwc_type =="pearson"){
         dud <- is.nan(localstats1$SDF$Corr_SENIOR_POPULATION.COMMUNITY_CLUBS)
@@ -434,7 +475,7 @@ server <- function(input, output) {
           drawmap2(localstats1$SDF[!dud,],localstats1$SDF$Spearman_rho_SENIOR_POPULATION.ELDERCARE[!dud])
         )
       }
-    }else {
+    }else if (gwc_data =="Residents Committee"){
       if(gwc_type =="pearson"){
         dud <- is.nan(localstats1$SDF$Corr_SENIOR_POPULATION.RC)
         output$gwcmap <- renderPlot(
@@ -444,6 +485,18 @@ server <- function(input, output) {
         dud <- is.nan(localstats1$SDF$Spearman_rho_SENIOR_POPULATION.RC)
         output$gwcmap <- renderPlot(
           drawmap2(localstats1$SDF[!dud,],localstats1$SDF$Spearman_rho_senior_pop.RC[!dud])
+        )
+      }
+    }else {
+      if(gwc_type =="pearson"){
+        dud <- is.nan(localstats1$SDF$Corr_SENIOR_POPULATION.GYM)
+        output$gwcmap <- renderPlot(
+          drawmap2(localstats1$SDF[!dud,],localstats1$SDF$Corr_SENIOR_POPULATION.GYM[!dud])
+        )
+      }else{
+        dud <- is.nan(localstats1$SDF$Spearman_rho_SENIOR_POPULATION.GYM)
+        output$gwcmap <- renderPlot(
+          drawmap2(localstats1$SDF[!dud,],localstats1$SDF$Spearman_rho_senior_pop.GYM[!dud])
         )
       }
     }
